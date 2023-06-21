590fb4ec22843037711999592248762be7b17a88
Patricio Uskaer <patriciouskaer@gmail.com>
2023-06-21 02:03:59 -0300

    about

--- src/components/About.js
+++ src/components/About.js
@@ -28,7 +28,7 @@ const About = () => {
           <div className='flex flex-col items-center text-center lg:items-start lg:text-left'>
             <div className='flex flex-col'>
               <h2 className='text-3xl lg:text-4xl font-medium lg:font-extrabold mb-3 before:content-about relative before:absolute before:opacity-40 before:-top-[2rem] before:hidden before:lg:block'>
-                Patricio Uskaer
+                Me
               </h2>
               <p className='mb-4 text-accent'>
                 FullStack Web Developer
