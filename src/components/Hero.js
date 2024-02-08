16dfd7ae7daec571d45c8dbb533c0ac73137b757
Patricio Uskaer <patriciouskaer@gmail.com>
2024-02-08 11:52:45 -0300

    coment pic

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -22,7 +22,7 @@ const Hero = () => {
             
             <div className='name-pic-div mr-32 text-center'>
               <div className='circlePic-div md:mr-10'>
-              <img className='w-40 hidden md:flex-row  md:w-60 md:block rounded-full lg:items-center' src={PatImg} alt='' />
+              {/* <img className='w-40 hidden md:flex-row  md:w-60 md:block rounded-full lg:items-center' src={PatImg} alt='' /> */}
               </div>
               <h1 className='text-center ml-10 text-4xl leading-[44px] md:text-5xl md:leading-tight lg:text-6xl font-bold md:tracking-[-2px]'>
                 <br /> Full Stack Developer
